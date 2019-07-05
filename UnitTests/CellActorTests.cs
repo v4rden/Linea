92015e4ce6833957dbea05892355df41f6be4bab
v4rden <denys.variah@gmail.com>
2019-07-06 02:59:31 +0300

    Added unit test project using nunit. Added CellActor test for clear method.

---
+++ UnitTests/CellActorTests.cs
@@ -0,0 +1,24 @@
+﻿namespace UnitTests
+{
+    using Akka.Actor;
+    using Akka.TestKit.NUnit3;
+    using Linea.ActorModel.Actors;
+    using Linea.ActorModel.Messges;
+    using Linea.Shared;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CellActorTests : TestKit
+    {
+        [Test]
+        public void Clear_GotClearMessage_StateSetToEmpty()
+        {
+            var prop = Props.Create(() => new CellActor(1, 1));
+            var cellActor = ActorOfAsTestActorRef<CellActor>(prop);
+
+            cellActor.Tell(new ClearMessage());
+
+            Assert.AreEqual(CellState.Empty, cellActor.UnderlyingActor.State);
+        }
+    }
+}
