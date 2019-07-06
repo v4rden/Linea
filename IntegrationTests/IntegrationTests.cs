904dfe895aa7641d39adb52265eaadf2397880cd
v4rden <denys.variah@gmail.com>
2019-07-06 06:24:40 +0300

    Added integration tests project

---
+++ IntegrationTests/IntegrationTests.cs
@@ -0,0 +1,14 @@
+﻿namespace IntegrationTests
+{
+    using NUnit.Framework;
+    
+    [TestFixture]
+    public class IntegrationTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
