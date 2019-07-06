274e1334ca22210b908e9f6e62ad8676e1c2b946
v4rden <denys.variah@gmail.com>
2019-07-06 05:35:14 +0300

    Implemented GetHashCode and Equals

--- Linea/Shared/CellLocation.cs
+++ Linea/Shared/CellLocation.cs
@@ -1,28 +1,54 @@
 namespace Linea.Shared
 {
     using System;
+    using Ninject;
 
     public class CellLocation
     {
-        public int Column { get; private set; }
-        public int Row { get; private set; }
+        public int Column { get; }
+        public int Row { get; }
 
         public CellLocation(int column, int row)
         {
-            if (column > CellLocationHelper.FieldLength)
-            {
-                throw new ArgumentException(
-                    $"Columnt {column} is greater than possible value of {CellLocationHelper.FieldLength}");
-            }
+            if(column < 0)
+                throw new ArgumentException("Column value can't be less than zero");
             
+            if(row< 0)
+                throw new ActivationException("Row value can't be less than zero");
+            
+            if (column > CellLocationHelper.FieldLength)
+                throw new ArgumentException(
+                    $"Column {column} is greater than possible value of {CellLocationHelper.FieldLength}");
+
             if (row > CellLocationHelper.FieldLength)
-            {
                 throw new ArgumentException(
                     $"Row {row} is greater than possible value of {CellLocationHelper.FieldLength}");
-            }
 
             Column = column;
             Row = row;
+        }
+
+
+        public override bool Equals(Object obj)
+        {
+            CellLocation cl = obj as CellLocation;
+            if (cl == null)
+                return false;
+
+            return Row == cl.Row && Column == cl.Column;
+        }
+
+        public bool Equals(CellLocation cl)
+        {
+            if (cl == null)
+                return false;
+
+            return Row == cl.Row && Column == cl.Column;
+        }
+
+        public override int GetHashCode()
+        {
+            return Row ^ Column;
         }
     }
 }
